f94cb52071dc27727c0f94758004157e33a0e766
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2024-02-20 18:49:34 +0000

    Updated project description

--- src/components/blogs/Overfitting.tsx
+++ src/components/blogs/Overfitting.tsx
@@ -1,9 +1,20 @@
-
+import React, {useLayoutEffect} from "react";
 
 const Overfitting = () => {
+
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0)
+    });
+
     return (
-        <div>
+        <div className="article">
             <div className="title-article">Overfitting</div>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
+                <figure>
+                    <img src="/src/assets/clip.png" height="400" width="auto" />
+                    <figcaption>Figure 1: CLIP - text-image foundation model.</figcaption>
+                </figure>
+                </div>
             <div className="abstract-article">
                 <div>
                     <p>Is it possible to quickly search through highly specified image data such as autonomous driving data and extract the most relevant samples with a text prompt?</p>
@@ -19,14 +30,48 @@ const Overfitting = () => {
             <div className="article-section">
                 <div className="section-title">Problem Statement</div>
                 <div className="section-description">
-                    <p>The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis:</p>
+                    <p>
+                        Nowadays autonomous driving is one of the most trendy in the tech industry. This resulted in a variety of investments in companies attempting to produce solutions to such problem. 
+                        This resulted in an increment in research, employees and amount of data used to build AI solutions. 
+                        However, the cost of data annotation is proportional to the amount of data. Moreover, in the autonomous driving the annotations usually
+                        revolve around complex tasks such as 3D object detection. Additionally, many companies consider multimodality which adds to the work necessary
+                        for data preparation. Therefore, the hypothesis for this project was the following:  
+                    </p>
+
+                    <p style={{textAlign: "center", fontWeight: "bold"}}>Is it possible to automatise to some degree the process of annotation for a highly specific data such as autonomous driving image dataset?</p>
+                
+                    <p>This project considered only the automatisation of image captioning. </p>
+                </div>
+            </div>
+            <div className="article-section">
+                <div className="section-title">Methods</div>
+                <div className="section-description">
+                    <p>
+                        The automatisation of image captioning is nothing else than connecting images with some text.
+                        For this task, there are many deep learning models that solve this task, but the most remarkable one
+                        is CLIP, an open-source model published by OpenAI [1]. 
+                    </p>
                 
                 </div>
             </div>
             <div className="article-section">
-                <div className="section-title">Methodology</div>
+                <div className="section-title">Results and Discussion</div>
                 <div className="section-description">
-                    <p>Methods used here are:</p>
+                    <p>Results used here are:</p>
+                
+                </div>
+            </div>
+            <div className="article-section">
+                <div className="section-title">Conclusions</div>
+                <div className="section-description">
+                    <p>Conclusions used here are:</p>
+                
+                </div>
+            </div>
+            <div className="article-section">
+                <div className="section-title">References</div>
+                <div className="section-description">
+                    <p>[1] - piwo</p>
                 
                 </div>
             </div>
--- src/components/projects/FrameSearchAPI.tsx
+++ src/components/projects/FrameSearchAPI.tsx
@@ -30,14 +30,27 @@ const FrameSearchAPI = () => {
             <div className="article-section">
                 <div className="section-title">Problem Statement</div>
                 <div className="section-description">
-                    <p>The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis: The project originated from the following hypothesis:</p>
+                    <p>
+                        Nowadays autonomous driving is one of the most trendy in the tech industry. This resulted in a variety of investments in companies attempting to produce solutions to such problem. 
+                        This resulted in an increment in research, employees and amount of data used to build AI solutions. 
+                        However, the cost of data annotation is proportional to the amount of data. Moreover, in the autonomous driving the annotations usually
+                        revolve around complex tasks such as 3D object detection. Additionally, many companies consider multimodality which adds to the work necessary
+                        for data preparation. Therefore, the hypothesis for this project was the following:  
+                    </p>
+
+                    <p style={{textAlign: "center", fontWeight: "bold"}}>Is it possible to automatise to some degree the process of annotation for a highly specific data such as autonomous driving image dataset?</p>
                 
+                    <p>This project considered only the automatisation of image captioning. </p>
                 </div>
             </div>
             <div className="article-section">
                 <div className="section-title">Methods</div>
                 <div className="section-description">
-                    <p>Methods used here are:</p>
+                    <p>
+                        The automatisation of image captioning is nothing else than connecting images with some text.
+                        For this task, there are many deep learning models that solve this task, but the most remarkable one
+                        is CLIP, an open-source model published by OpenAI [1]. 
+                    </p>
                 
                 </div>
             </div>
